eb3e7800863a99d7814af0e013c6ac376df5f0aa
BedguyHeck <113556458+BedguyHeck@users.noreply.github.com>
2025-10-15 17:49:19 +0000

    commit message

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -1,54 +1,60 @@
 package com.example.project;
-public class Calculator{
+public class Calculator {
     public String calcModel;
-    
     //constructor that creates an instance of the calculator class
     //the calculator class has one attirbute -> a string that holds the model name of the calculator 
     public Calculator(String x) {
-        //implement code here
+        this.calcModel = x;
     }
-    
     // method that performs the operation designated by operand (which
     // could be +, -, *, or /) and returns the result of that operation
     // performed between num1 and num2
     // NOTE: if operand is some String other than "+", "-", "*", or "/" then
     // this method should return 0
-    // REMINDER: use .equals to test if one string is equal to another (not ==)
     public double performOperation(String operand, int num1, int num2) {
-        //implement code here
-        return 0.00;
+        if (operand.equals("+")) {
+            return num1 + num2;
+        } else if (operand.equals("-")) {
+            return num1 - num2;
+        } else if (operand.equals("*")) {
+            return num1 * num2;
+        } else if (operand.equals("/")) {
+            
+            if (num2 != 0) {
+                return (double) num1 / num2;
+            } else {
+                return 0.00;
+            }
+        } else {
+            return 0.00;
+        }
     }
 
     // method that checks if num1 is evenly divisible by num2, and if so,
     // returns true, and if not, returns false
     public boolean divisibleBy(int num1, int num2) {
-        //implement code here
-        return false;
+        if (num2 == 0) return false; 
+        return num1 % num2 == 0;
     }
-
     // method that constructs and returns a string representing a coordinate
     // pair in the format: "(x, y)"
     public String coordinatePair(int x, int y) {
-        //implement code here
-        return "";
+        return "(" + x + ", " + y + ")";
     }
     // method that determines and returns the result of |num1 - num2|
     public int absoluteValue(int num1, int num2) {
-        //implement code here
-        return 0;
+        return Math.abs(num1 - num2);
     }
-
     //method that returns the average of two numbers. 
     //the number returned should be rounded to the nearest tenth
-    public double average(int num1, int num2){
-        return 0;
+    public double average(int num1, int num2) {
+        double avg = (num1 + num2) / 2.0;
+        return Math.round(avg * 10.0) / 10.0;
     }
-    
     // method that constructs and returns a String in the format:
     // "This calculator is a model "+____+" where "+____+" should be filled in
     // with the value stored in the calcModel instance variable."
     public String info() {
-        //implement code here
-        return "";
+        return "This calculator is a model " + calcModel;
     }
 }
--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -3,10 +3,35 @@ package com.example.project;
 public class CalculatorRunner {
     public static void main(String[] args) {
 
-        //test your program here
-        //1. create an instance of the calculator class
+        // 1. Create an instance of the Calculator class
+        Calculator myCalc = new Calculator("TI-84");
 
-        //2. call any methods of that class to test
+        // 2. Call methods of that class to test
+        System.out.println("Model Info: " + myCalc.info());
 
+        // Test performOperation
+        System.out.println("5 + 3 = " + myCalc.performOperation("+", 5, 3));
+        System.out.println("10 - 7 = " + myCalc.performOperation("-", 10, 7));
+        System.out.println("4 * 6 = " + myCalc.performOperation("*", 4, 6));
+        System.out.println("8 / 2 = " + myCalc.performOperation("/", 8, 2));
+        System.out.println("Division by zero: " + myCalc.performOperation("/", 8, 0));
+        System.out.println("Invalid operation: " + myCalc.performOperation("%", 5, 2));
+
+        // Test divisibleBy
+        System.out.println("10 divisible by 2? " + myCalc.divisibleBy(10, 2));
+        System.out.println("10 divisible by 3? " + myCalc.divisibleBy(10, 3));
+        System.out.println("Divide by zero check: " + myCalc.divisibleBy(10, 0));
+
+        // Test coordinatePair
+        System.out.println("Coordinate pair: " + myCalc.coordinatePair(4, 7));
+
+        // Test absoluteValue
+        System.out.println("Absolute value of 10 - 4: " + myCalc.absoluteValue(10, 4));
+        System.out.println("Absolute value of 4 - 10: " + myCalc.absoluteValue(4, 10));
+
+        // Test average
+        System.out.println("Average of 5 and 9: " + myCalc.average(5, 9));
+        System.out.println("Average of 2 and 3: " + myCalc.average(2, 3));
+        System.out.println("Average of 1 and 2: " + myCalc.average(1, 2));
     }
 }
